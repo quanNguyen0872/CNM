2285900d1a28b70fbecc26c6d9085140d1fb631f
quanNguyen0872 <quan14139@gmail.com>
2022-10-13 12:51:47 +0700

    firstCommit

---
+++ webchatapp/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyAURuFpMUKMgCC3wkLITXDIpPeh4xBcNes",
+	authDomain: "chat-app-cnm-dc5b8.firebaseapp.com",
+	databaseURL: "https://chat-app-cnm-dc5b8-default-rtdb.asia-southeast1.firebasedatabase.app",
+	projectId: "chat-app-cnm-dc5b8",
+	storageBucket: "chat-app-cnm-dc5b8.appspot.com",
+	messagingSenderId: "785710641310",
+	appId: "1:785710641310:web:a276dafa5ff60bb27bcb8f",
+	measurementId: "G-L81X11C0QY"
+  };
